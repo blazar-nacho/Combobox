2b3a6c4c606ab0906c87fb14f89cf3fbc161ff2f
sodafanser <sodafanser@gmail.com>
2015-06-17 20:13:07 -0300

    un arreglo

--- Combobox/Sprites.h
+++ Combobox/Sprites.h
@@ -50,7 +50,7 @@ private:
 	std::vector<SDL_Rect*>* Tumbado;
 	std::vector<SDL_Rect*>* Subbarrida;
 	//XjoseCargaSprite 4
-	std::vector<SDL_Rect*>* Subbarrida;
+	
 	std::vector<SDL_Rect*>* Dizzy;
 	std::vector<SDL_Rect*>* Bicicleta;
 	std::vector<SDL_Rect*>* Flykick;
